5b2a056a65c42ff87324872089f76eb50b4131bb
Bartłomiej Fraszczyk <bartek.fraszczyk@gmail.com>
2023-08-20 21:16:47 +0200

    Version bump

--- skrypty.lua
+++ skrypty.lua
@@ -1,4 +1,4 @@
-scripts = scripts or { ver = "2.0.3" }
+scripts = scripts or { ver = "2.1.0" }
 scripts["event_handlers"] = scripts["event_handlers"] or {}
 scripts["utils"] = {}
 scripts.character_name = ""
