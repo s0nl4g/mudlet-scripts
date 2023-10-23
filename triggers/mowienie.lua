cdc849ea63a4e4228ff0fe07897a225f3f0fd77f
buczbucz <mateuszbukaty@gmail.com>
2023-10-23 11:40:42 +0200

    Zmiana koloru szeptania.
    1. Kolor szeptów z blue na dodger_blue żeby było bardziej widoczne.
    
    2. Dodanie tego samego koloru również do szeptów innych osób.
    
    3. Brakujący reset format.

--- triggers/mowienie.lua
+++ triggers/mowienie.lua
@@ -1,34 +1,40 @@
 function trigger_mowienie_func()
-    local kto = matches[2]
-    local jak = matches[4]
-    local cel = matches[6]
-    local tekst = matches[7]
-    --display(matches)
-    selectCurrentLine()
-    fg("cyan")
-    
-    if(cel == "ciebie") then
-        selectCaptureGroup(6)
-        fg("white")
-    end
-    chatConsole:cecho("[" .. getTime(true, "HH:mm") .. "] <cyan>" .. kto .. jak .. (cel ~= "" and (" do " .. cel) or ("")) .. ": <white>" .. tekst .. "<reset>\n")
-    scripts.windows:flashChatContainerButton(chatContainer)
-    resetFormat()    
+	local kto = matches[2]
+	local co = matches[3]
+	local jak = matches[4]
+	local cel = matches[6]
+	local tekst = matches[7]
+	--display(matches)
+	selectCurrentLine()
+	if(co == "szepcze") then
+		fg("dodger_blue")
+	else
+		fg("cyan")
+	end
+
+	if(cel == "ciebie") then
+		selectCaptureGroup(6)
+		fg("white")
+	end
+	chatConsole:cecho("[" .. getTime(true, "HH:mm") .. "] <cyan>" .. kto .. jak .. (cel ~= "" and (" do " .. cel) or ("")) .. ": <white>" .. tekst .. "<reset>\n")
+	scripts.windows:flashChatContainerButton(chatContainer)
+	resetFormat()
 end
 
 function trigger_ja_mowie_func()
-    selectCurrentLine()
-    --display(matches)
-    
-    local co = matches[2]
-    local jak = matches[3]
-    local cel = matches[5]
-    local tekst = matches[6]
-    if(co == "Szepczesz") then
-        fg("blue")
-    else
-        fg("cyan")
-    end
-    chatConsole:cecho("[" .. getTime(true, "HH:mm") .. "] <cyan>" .. co .. jak .. (cel ~= "" and (" do " .. cel) or ("")) .. ": <white>" .. tekst .. "<reset>\n")
-    scripts.windows:flashChatContainerButton(chatContainer)    
+	selectCurrentLine()
+	--display(matches)
+
+	local co = matches[2]
+	local jak = matches[3]
+	local cel = matches[5]
+	local tekst = matches[6]
+	if(co == "Szepczesz") then
+		fg("dodger_blue")
+	else
+		fg("cyan")
+	end
+	chatConsole:cecho("[" .. getTime(true, "HH:mm") .. "] <cyan>" .. co .. jak .. (cel ~= "" and (" do " .. cel) or ("")) .. ": <white>" .. tekst .. "<reset>\n")
+	scripts.windows:flashChatContainerButton(chatContainer)
+	resetFormat()
 end
