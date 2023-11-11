910b0967071b702a2a60ccaf220b03b0d31dc2f5
buczbucz <mateuszbukaty@gmail.com>
2023-11-11 19:28:14 +0100

    Version bump.

--- skrypty.lua
+++ skrypty.lua
@@ -1,4 +1,4 @@
-scripts = scripts or { ver = "2.1.4" }
+scripts = scripts or { ver = "2.1.5" }
 scripts["event_handlers"] = scripts["event_handlers"] or {}
 scripts["utils"] = {}
 scripts.character_name = ""
