da58af0750c930c91e848574c1bf75747c10713a
Karol Lassak <ingwar@ingwar.eu.org>
2023-12-02 15:24:38 +0100

    Podbicie wersji

--- skrypty.lua
+++ skrypty.lua
@@ -1,4 +1,4 @@
-scripts = scripts or { ver = "2.1.7" }
+scripts = scripts or { ver = "2.1.8" }
 scripts["event_handlers"] = scripts["event_handlers"] or {}
 scripts["utils"] = {}
 scripts.character_name = ""
